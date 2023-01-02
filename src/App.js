f6eff4d63a5ad090fdb54c919fd32e64fbec0f13
Gaurav_Singh <gauravkarki038@gmail.com>
2023-01-02 21:44:21 +0530

    user List

--- src/App.js
+++ src/App.js
@@ -1,9 +1,84 @@
+import { useContext } from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { DarkModeContext } from "./context/darkModeContext";
 import './App.css';
+import { AuthContext } from './context/AuthContext';
 
 function App() {
+
+  const {darkMode} = useContext(DarkModeContext );
+
+  const ProtectedRoute = ({children}) => {
+    const {user} = useContext(AuthContext);
+    if(!user){
+      return <Navigate to="/login" />;
+    }
+    return children;
+
+  }
   return (
-    <div className="App">
-     hellooo
+    <div className={darkMode ? "app dark" : "app"}>
+     <BrowserRouter>
+     <Routes>
+          <Route path="/">
+            <Route path="login" element={<Login />} />
+            <Route path="users">
+              <Route
+                index
+                element={
+                  <ProtectedRoute>
+                    <List columns={userColumns} />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="new"
+                element={
+                  <ProtectedRoute>
+                    <New inputs={userInputs} title="Add New User" />
+                  </ProtectedRoute>
+                }
+              />
+            </Route>
+            <Route path="hotels">
+              <Route
+                index
+                element={
+                  <ProtectedRoute>
+                    <List columns={hotelColumns} />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="new"
+                element={
+                  <ProtectedRoute>
+                    <NewHotel  />
+                  </ProtectedRoute>
+                }
+              />
+            </Route>
+            <Route path="rooms">
+              <Route
+                index
+                element={
+                  <ProtectedRoute>
+                    <List columns={roomColumns} />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="new"
+                element={
+                  <ProtectedRoute>
+                    <NewRoom  />
+                  </ProtectedRoute>
+                }
+              />
+            </Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
